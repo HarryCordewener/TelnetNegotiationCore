42797cccaab38d3f1e44453b0fc0766b5f4bd27d
Harry Cordewener <admin@twilightdays.org>
2025-03-16 22:44:02 -0500

    F# adjustments.

--- TelnetNegotiationCore.Functional/MSDPLibrary.fs
+++ TelnetNegotiationCore.Functional/MSDPLibrary.fs
@@ -15,41 +15,46 @@ module MSDPLibrary =
         | MSDP_ARRAY_CLOSE = 6uy
 
     let emptyMap: Map<string, obj> = Map<string, obj> []
-    
-    let MSDPVal: byte = byte Trigger.MSDP_VAL
+
+    let MSDP_VAL: byte = byte Trigger.MSDP_VAL
+
+    let MSDP_VAR: byte = byte Trigger.MSDP_VAR
+
+    let MSDP_ARRAY_OPEN: byte = byte Trigger.MSDP_ARRAY_OPEN
+
+    let MSDP_ARRAY_CLOSE: byte = byte Trigger.MSDP_ARRAY_CLOSE
+
+    let MSDP_TABLE_OPEN: byte = byte Trigger.MSDP_TABLE_OPEN
+
+    let MSDP_TABLE_CLOSE: byte = byte Trigger.MSDP_TABLE_CLOSE
 
     [<TailCall>]
     let rec private MSDPScanTailRec (root: obj, array: byte seq, encoding: Encoding) =
         let rec scanRec accRoot accArray =
-            if Seq.length accArray = 0 then
-                (accRoot, accArray)
-            else
-                match accArray |> Seq.head with
-                | 1uy ->
-                    let key =
-                        encoding.GetString(
-                            accArray |> Seq.skip 1 |> Seq.takeWhile (fun x -> x <> MSDPVal) |> Array.ofSeq
-                        )
+            match accArray |> Seq.tryHead with
+            | None -> accRoot, accArray
+            | Some(1uy) ->
+                let key =
+                    encoding.GetString(accArray |> Seq.skip 1 |> Seq.takeWhile (fun x -> x <> MSDP_VAL) |> Array.ofSeq)
 
-                    let cv, rest =
-                        scanRec root (accArray |> Seq.skip 1 |> Seq.skipWhile (fun x -> x <> MSDPVal))
+                let cv, rest =
+                    scanRec root (accArray |> Seq.skip 1 |> Seq.skipWhile (fun x -> x <> MSDP_VAL))
 
-                    scanRec ((accRoot :?> Map<string, obj>).Add(key, cv)) rest
-                | 2uy ->
-                    match accRoot with
-                    | :? Map<string, obj> -> scanRec emptyMap (accArray |> Seq.skip 1)
-                    | :? List<obj> ->
-                        let cv, rest = scanRec emptyMap (accArray |> Seq.skip 1)
-
-                        scanRec ((accRoot :?> List<obj>) @ [ cv ]) rest
-                    | _ -> scanRec accRoot (accArray |> Seq.skip 1)
-                | 3uy -> scanRec emptyMap (accArray |> Seq.skip 1)
-                | 4uy -> accRoot, accArray |> Seq.skip 1
-                | 5uy -> scanRec [] (accArray |> Seq.skip 1)
-                | 6uy -> accRoot, accArray |> Seq.skip 1
-                | _ ->
-                    encoding.GetString(accArray |> Seq.takeWhile (fun x -> x > 6uy) |> Array.ofSeq),
-                    accArray |> Seq.skipWhile (fun x -> x > 6uy)
+                scanRec ((accRoot :?> Map<string, obj>).Add(key, cv)) rest
+            | Some(2uy) ->
+                match accRoot with
+                | :? Map<string, obj> -> scanRec emptyMap (accArray |> Seq.skip 1)
+                | :? List<obj> as accRootList ->
+                    let cv, rest = scanRec emptyMap (accArray |> Seq.skip 1)
+                    scanRec (accRootList @ [ cv ]) rest
+                | _ -> scanRec accRoot (accArray |> Seq.skip 1)
+            | Some(3uy) -> scanRec emptyMap (accArray |> Seq.skip 1)
+            | Some(4uy) -> accRoot, accArray |> Seq.skip 1
+            | Some(5uy) -> scanRec [] (accArray |> Seq.skip 1)
+            | Some(6uy) -> accRoot, accArray |> Seq.skip 1
+            | _ ->
+                encoding.GetString(accArray |> Seq.takeWhile (fun x -> x > 6uy) |> Array.ofSeq),
+                accArray |> Seq.skipWhile (fun x -> x > 6uy)
 
         scanRec root array
 
@@ -64,21 +69,21 @@ module MSDPLibrary =
                 let parsedObj =
                     jsonNode.AsObject()
                     |> Seq.map (fun prop ->
-                        [ byte Trigger.MSDP_VAR ]
+                        [ MSDP_VAR ]
                         @ (encoding.GetBytes(prop.Key) |> List.ofArray)
-                        @ [ byte Trigger.MSDP_VAL ]
+                        @ [ MSDP_VAL ]
                         @ parseJsonValue prop.Value)
                     |> List.concat
 
-                [ byte Trigger.MSDP_TABLE_OPEN ] @ parsedObj @ [ byte Trigger.MSDP_TABLE_CLOSE ]
+                [ MSDP_TABLE_OPEN ] @ parsedObj @ [ MSDP_TABLE_CLOSE ]
             | JsonValueKind.Array ->
                 let parsedArr =
                     jsonNode.AsArray()
-                    |> Seq.map (fun prop -> [ byte Trigger.MSDP_VAL ] @ parseJsonValue prop)
+                    |> Seq.map (fun prop -> [ MSDP_VAL ] @ parseJsonValue prop)
                     |> List.ofSeq
                     |> List.concat
 
-                [ byte Trigger.MSDP_ARRAY_OPEN ] @ parsedArr @ [ byte Trigger.MSDP_ARRAY_CLOSE ]
+                [ MSDP_ARRAY_OPEN ] @ parsedArr @ [ MSDP_ARRAY_CLOSE ]
             | JsonValueKind.String -> encoding.GetBytes(jsonNode.AsValue().ToString()) |> List.ofArray
             | JsonValueKind.Number -> encoding.GetBytes(jsonNode.AsValue().ToString()) |> List.ofArray
             | JsonValueKind.True -> encoding.GetBytes("1") |> List.ofArray
